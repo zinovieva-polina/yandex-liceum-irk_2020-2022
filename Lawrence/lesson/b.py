69360024038eb326a16bf50f249da90d5dd6f108
prostoLavr <prostoLawr@gmail.com>
2021-12-04 16:37:26 +0800

    b.py by Lawrence

---
+++ Lawrence/lesson/b.py
@@ -0,0 +1,40 @@
+import pygame, sys
+import random 
+
+
+def main():
+    coord = []
+    pygame.init()
+    width, height = random.randint(150, 800), random.randint(150, 800)
+    size = 25
+    screen = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                coord.append((list(event.pos), [-1, 1]))
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                return  
+
+        screen.fill((0, 0, 200))
+        
+        for num, (pos, vec) in enumerate(coord):
+            pos[0] += vec[0]
+            pos[1] += vec[1]
+            for i in range(2):
+                if pos[i] > width or pos[i] < 0:
+                    vec[i] = -vec[i]
+            rect = (pos[0] - size // 2, pos[1] - size // 2, size,  size)
+            pygame.draw.ellipse(screen, (200, 200, 0), rect) 
+            coord[num] = pos, vec
+
+        pygame.display.update()
+
+        dt = clock.tick(60)
+
+if __name__ == '__main__':
+    main()
