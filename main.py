9b2067a428436d4781a073f7665cb2d1abb55608
Polina <polyzin98@gmail.com>
2021-12-11 10:41:25 +0300

    create main and change superfunc

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_superfunction():
+    print('Hello, python!!!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
