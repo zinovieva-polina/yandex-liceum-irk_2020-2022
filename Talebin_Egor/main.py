c4d56a0115b347fa1793086953400f36035174fc
инопришиленец <etalebin@yandex.ru>
2021-12-11 15:34:39 +0800

    Talebin_Egor

--- Talebin_Egor/main.py
+++ Talebin_Egor/main.py
@@ -1,48 +1,89 @@
 import pygame
+import sys
 
-size = width, height = 800, 800
+width, height = list(map(int, "250 250".split()))
+size = width, height
+pygame.init()
+size_block = 50
 screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
 
-running = True
+margin = 15
+blue = (255, 0, 255)
+white = (0, 255, 255)
+red = (255, 255, 0)
+pygame.display.set_caption('Tic tac toe')
+mas = [[0] * 5 for i in range(3)]
+query = 0
 
-screen2 = pygame.Surface(screen.get_size())
-x1, y1 = 10, 10
-drawing = False
-while running:
+
+
+def check_win(mas):
+    # x
+    if mas[0][0] == 'x' and mas[0][1] == 'x' and mas[0][2] == 'x':
+        print('Крестики выиграли')
+    if mas[1][0] == 'x' and mas[1][1] == 'x' and mas[1][2] == 'x':
+        print('Крестики выиграли')
+    if mas[2][0] == 'x' and mas[2][1] == 'x' and mas[2][2] == 'x':
+        print('Крестики выиграли')
+    #
+    if mas[0][0] == 'x' and mas[1][0] == 'x' and mas[2][0] == 'x':
+        print('Крестики выиграли')
+    if mas[0][1] == 'x' and mas[1][1] == 'x' and mas[2][1] == 'x':
+        print('Крестики выиграли')
+    if mas[0][2] == 'x' and mas[1][2] == 'x' and mas[2][2] == 'x':
+        print('Крестики выиграли')
+    #
+    if mas[0][0] == 'x' and mas[1][1] == 'x' and mas[2][2] == 'x':
+        print('Крестики выиграли')
+    if mas[0][2] == 'x' and mas[1][1] == 'x' and mas[2][0] == 'x':
+        print('Крестики выиграли')
+    # o
+    if mas[0][0] == 'o' and mas[0][1] == 'o' and mas[0][2] == 'o':
+        print('Нолики выиграли')
+    if mas[1][0] == 'o' and mas[1][1] == 'o' and mas[1][2] == 'o':
+        print('Нолики выиграли')
+    if mas[2][0] == 'o' and mas[2][1] == 'o' and mas[2][2] == 'o':
+        print('Нолики выиграли')
+    #
+    if mas[0][0] == 'o' and mas[1][0] == 'o' and mas[2][0] == 'o':
+        print('Нолики выиграли')
+    if mas[0][1] == 'o' and mas[1][1] == 'o' and mas[2][1] == 'o':
+        print('Нолики выиграли')
+    if mas[0][2] == 'o' and mas[1][2] == 'o' and mas[2][2] == 'o':
+        print('Нолики выиграли')
+    #
+    if mas[0][0] == 'o' and mas[1][1] == 'o' and mas[2][2] == 'o':
+        print('Нолики выиграли')
+    if mas[0][2] == 'o' and mas[1][1] == 'o' and mas[2][0] == 'o':
+        print('Нолики выиграли')
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            drawing = True
-            x1, y1 = event.pos
-        if event.type == pygame.MOUSEBUTTONUP:
-            screen2.blit(screen, (0, 0))
-            drawing = False
-        if event.type == pygame.MOUSEMOTION:
-            w, h = event.pos[0] - x1, event.pos[1] - y1
-            u, i = event.pos[0] - x1, event.pos[1] - y1
-    screen.fill(pygame.Color('black'))
-    screen.blit(screen2, (0, 0))
-    if drawing:
-        pygame.draw.rect(screen, (0, 255, 0), ((x1, y1), (w, h)), 5)
-
-while running:
-    for event in pygame.event.get():
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            x1 += 1
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            x1 -= 1
-            x1, y1 = event.pos
-        if event.type == pygame.MOUSEBUTTONUP:
-            screen2.blit(screen, (0, 0))
-            drawing = False
-        if event.type == pygame.MOUSEMOTION:
-            u, i = event.pos[0] - x1, event.pos[1] - y1
-            screen.fill(pygame.Color('black'))
-            screen.blit(screen2, (0, 0))
-            if drawing:
-                pygame.draw.rect(screen, (255, 255, 255), ((x1, y1), (u, i)), 5)
-
-    pygame.display.flip()
-pygame.quit()
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
+            if mas[row][col] == 0:
+                if query % 2 == 0:
+                    mas[row][col] = 'x'
+                else:
+                    mas[row][col] = 'o'
+            query += 1
+    for row in range(3):
+        for col in range(3):
+            x = col * size_block + (col + 1) * margin
+            y = row * size_block + (row + 1) * margin
+            pygame.draw.rect(screen, blue, (x, y, size_block, size_block))
+            if mas[row][col] == 'x':
+                pygame.draw.line(screen, red, (x + 5, y + 5), (x + size_block - 5, y + size_block - 5), 8)
+                pygame.draw.line(screen, red, (x + size_block - 5, y + 5), (x + 5, y + size_block - 5), 8)
+            elif mas[row][col] == 'o':
+                pygame.draw.circle(screen, red, (x + size_block // 2, y + size_block // 2), size_block // 2 - 3, 8)
+            #
+            check_win(mas)
+            if query == 9:
+                print('ничЬя')
+    pygame.display.update()
--- Talebin_Egor/test.py
+++ Talebin_Egor/test.py
@@ -4,17 +4,25 @@ if __name__ == '__main__':
     global pos
     pygame.init()
 
-width, height = list(map(int, input(":").split()))
+width, height = list(map(int, "800 800".split()))
 size = width, height
 screen = pygame.display.set_mode(size)
 
 running = True
-st1 = 0
+st1 = float(0)
 st2 = 1
+x = 0
+y = 0
+z = 0
+n = 0
 clock = pygame.time.Clock()
 
 while running:
-    screen.fill((255, 255, 255))
+    if x >= 255:
+        x = 0
+        y = 0
+        z = 0
+    screen.fill((150, 150, 150))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -23,16 +31,42 @@ while running:
             st1 = 0
             st2 = 0
     while st2 != 1:
-        pygame.draw.circle(screen, (255, 255, 0), pos, st1)
-        pygame.display.flip()
-        st1 += 10
+        if x != 255 and y == 0 and z == 0:
+            pygame.draw.circle(screen, (x, y, z), pos, st1)
+            pygame.display.flip()
+            st1 += 5
+            x += 5
+            n = 0
+
+        if x == 255 and y != 255 and z == 0:
+            pygame.draw.circle(screen, (x, y, z), pos, st1)
+            pygame.display.flip()
+            st1 -= 5
+            y += 5
+            n = 0
+
+        if x == 255 and y == 255 and z != 255:
+            pygame.draw.circle(screen, (x, y, z), pos, st1)
+            pygame.display.flip()
+            st1 += 5
+            z += 5
+            n += 1
+
+        if x != 0 and y != 0 and z != 0 and n >= 51:
+            pygame.draw.circle(screen, (x, y, z), pos, st1)
+            pygame.display.flip()
+            x -= 5
+            y -= 5
+            z -= 5
+            st1 -= 5
+
         clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 st2 = 1
             if event.type == pygame.MOUSEBUTTONDOWN:
-                screen.fill((255, 255, 255))
+                screen.fill((150, 150, 150))
                 pygame.display.flip()
                 pos = event.pos
                 st1 = 0
