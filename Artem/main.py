2b2424636b9f0a964cb3e6f68004c8529fb01f7f
AdminYoung21 <artem.klepikov.19@mail.ru>
2021-12-11 15:22:25 +0800

    ветка

--- Artem/main.py
+++ Artem/main.py
@@ -1,22 +1,14 @@
-import pygame
+def my_superfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('Artem git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
 
 if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
-
-    running = True
-    y_pos = 0
-    v = 100  # пикселей в секунду
-    clock = pygame.time.Clock()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill((50, 205, 50))
-        pygame.draw.rect(screen, (255, 255, 255), (300, 0, 200, 1000))
-        pygame.draw.circle(screen, (255, 0, 0), (400, y_pos), 30)
-        y_pos += v * clock.tick() / 1000  # v * t в секундах
-        pygame.display.flip()
-    pygame.quit()
+    main()
